70a6d88b7021fe2d631322dc2f6b9b9e985663ca
Gergely Andras Szabo <szagege@gmail.com>
2019-10-03 09:42:46 +0200

    build(angular): update to angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {
-  MatButtonModule, MatButtonToggleModule,
-  MatCardModule,
-  MatChipsModule, MatDividerModule,
-  MatExpansionModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatRadioModule
-} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
@@ -19,9 +19,9 @@ import {AngularFireDatabaseModule} from '@angular/fire/database';
 import {MainComponent} from './main/main.component';
 import {RouterModule, Routes} from '@angular/router';
 import {OrderComponent} from './order/order.component';
-import { PancakeObjectComponent } from './order/pancake-object/pancake-object.component';
+import {PancakeObjectComponent} from './order/pancake-object/pancake-object.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { OrdersComponent } from './orders/orders.component';
+import {OrdersComponent} from './orders/orders.component';
 
 const appRoutes: Routes = [
   {path: 'main', component: MainComponent},
